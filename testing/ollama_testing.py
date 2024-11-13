e844a2cb29e35596589a6b4e199bcf723e7006f0
SaishaShetty <spshetty@ucdavis.edu>
2024-11-13 00:06:47 +0000

    changes

--- testing/ollama_testing.py
+++ testing/ollama_testing.py
@@ -1,27 +1,23 @@
 import requests
 import json
 
-report_text = "Evaluate for effusions . There is mild - to - moderate cardiomegaly . Bilateral pleural effusions are small . Aside from atelectasis in the left lower lobe , the lungs are grossly clear . Almost complete resolution of atelectasis in the left upper lobe . Sternal wires are aligned . Widened mediastinum has improved . A small air - fluid level in the retrosternal region suggests the presence of a tiny pneumothorax and small effusion . These are most likely located in the left side . "
+report_text = 'The lungs are clear . Cardiomediastinal and hilar contours are normal . There are no pleural effusions or pneumothorax .'
 prompt = f"""
-<|begin_of_text|><|start_header_id|>Assistant<|end_header_id|>
-     Your task is to extract anatomy terms exactly as they appear in the report. Do not modify, infer, or shorten terms in any way. Only output terms that appear word-for-word in the text. Go through each individual word.
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+You are an expert radiology assistant that helps annotate reports<|eot_id|><|start_header_id|>user<|end_header_id|>
+Follow these guidelines:\n
+1. Label terms as anatomy if they clearly refer to a body part and, if present, include any modifier and descriptors.\n
+2. Label terms as observation if they describe a state, condition, or finding related to the anatomy.\n
+Do not label:\n
+    1. Medical devices\n
+    2. Medical conditions or descriptive terms related to conditions (usually an observation)\n
+    3. Adjectives describing an anatomy part\n
 
-    [THOUGHT_START]
-    The terms should solely be an anatomy in the body. **Provide proper reasoning for why you labelled each of them as an anatomy**. 
-    If your reason does not indicate it is a anatomy part only then you should not label it. If the anatomy term has a modifier include those as well. 
-    MAKE SURE TO NOT label these terms as anatomy:
-    1. Medical device
-    2. Medical condition or description of a condition
-    3. An adjactive describing an anatomy part
-    
-    If a label includes two words, separate them but still keep them as labels.
-    Once that is done, give me the final list of terms that exactly appear in the report.
-    [THOUGHT_END]
-
+Label each word seperately. Go through every word.
     Report: {report_text}
-
-    Output:
-    [ANATOMY] term1, term2  
+<|eot_id|><|start_header_id|>user<|end_header_id|>
+ Provide the annotated list with proper reasoning for each: <|start_header_id|>Assistant<|end_header_id|>
+   
 <|end_of_text|>
     """
 
