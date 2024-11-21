60baafca6d8043e807fd5812a14be6d9a8b0e401
SaishaShetty <spshetty@ucdavis.edu>
2024-11-21 04:56:07 +0000

    score eval added

--- data/mixtral/download.py
+++ data/mixtral/download.py
@@ -1,14 +1,32 @@
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from rouge_score import rouge_scorer
+from bert_score import score
 
-# Initialize the text generation pipeline with the Mistral model
-model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-pipe = pipeline("text-generation", model=model_name)
-# Define chat messages with roles and content
-messages = [
-    {"role": "system", "content": "You are a radiological expert capable of generating diverse synthetic clinical reports and their annotations. Your task is to create variations in findings, conditions, and anatomical structures related to the chest to help build a comprehensive dataset."},
-    {"role": "user", "content": "Explain the concept of artificial intelligence."},
-]
-# Generate response using the pipeline
-response = pipe(messages, max_new_tokens=128)[0]['generated_text']
-# Print the assistant's response
-print(response)
+def compute_rouge_l(reference, hypothesis):
+    scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=True)
+    scores = scorer.score(reference, hypothesis)
+    return scores["rougeL"]
+
+def compute_bert_score(reference, hypothesis):
+    P, R, F1 = score([hypothesis], [reference], lang="en")
+    return P,R,F1
+
+# Example usage
+reference_text = "Patient has been extubated . Lungs are clear . Normal cardiomediastinal and hilar silhouettes and pleural surfaces ."
+hypothesis_text = ["Patient has a history of smoking. Significant calcification observed in the apical segment of the left lower lobe.",
+"Patient presents with persistent chest pain. No signs of abnormalities in the heart or major vessels. Pleural effusion is noted on the left side.",
+"Patient's CT scan shows a possible nodule in the right upper lobe. Further evaluation recommended.", 
+"Patient has a normal EKG. No signs of ischemia or infarction.",
+"Patient presents with shortness of breath. There is a mass in the right lung apex."]
+
+for s in hypothesis_text:
+    rouge_l_score = compute_rouge_l(reference_text, s)
+    print("ROUGE-L Precision: {:.2f}".format(rouge_l_score.precision))
+    print("ROUGE-L Recall: {:.2f}".format(rouge_l_score.recall))
+    print("ROUGE-L F1 Score: {:.2f}".format(rouge_l_score.fmeasure))
+
+
+for s in hypothesis_text:
+    P, R, F1 = compute_bert_score(reference_text, s)
+    print("BERTScore Precision:", P.mean().item())
+    print("BERTScore Recall:", R.mean().item())
+    print("BERTScore F1:", F1.mean().item())
