d1a623d49e15892ba12abf5d459e3a494269241c
SaishaShetty <spshetty@ucdavis.edu>
2025-05-07 04:55:34 +0000

    Preprocessing files added

--- data_generation/preprocess/length.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Load JSON from a file
-with open("/home/spshetty/RadAnnotate/data_generation/outputs/check.json", "r") as file:
-    data = json.load(file)  # This will load the JSON as a Python list if it's in the format [{},{}]
-
-# Check the number of dictionaries in the list
-num_dicts = len(data)
-print(f"Number of dictionaries: {num_dicts}")
